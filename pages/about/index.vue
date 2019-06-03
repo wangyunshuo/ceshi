1095bbc2f27474de00907a0bdda262d9d0f7247a
wangyunshuo <wangyunshuo@qq.com>
2019-06-03 16:09:31 +0800

    cs

---
+++ pages/about/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    about
+    <p>
+      <nuxt-link :to="{name:'index'}">
+        首页
+      </nuxt-link>
+    </p>
+  </div>
+</template>
---
+++ pages/news/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>new</h1>
+    <a href="/news/new1">new1</a>
+    <nuxt-link :to="{name:'news-string',params:{string:'new2'}}">
+      new2
+    </nuxt-link>
+  </div>
+</template>
